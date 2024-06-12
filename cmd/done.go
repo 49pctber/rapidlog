b435e16bf861563e5f521a3f1aa5eef6145e72d0
Bryan Redd <bryan.d.redd@gmail.com>
2024-06-11 22:20:43 -0600

    Added todo and done features

---
+++ cmd/done.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	rapidlog "github.com/49pctber/rapidlog/internal"
+	"github.com/spf13/cobra"
+)
+
+// doneCmd represents the done command
+var doneCmd = &cobra.Command{
+	Use:   "done",
+	Short: "mark a todo list item as done",
+	Long:  `mark a todo list item as done`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		entry, err := rapidlog.GetEntry(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		if entry.Type != "." {
+			fmt.Printf("entry not a todo item (type %s, not .)\n", entry.Type)
+			return
+		}
+
+		entry.Type = "x"
+
+		err = entry.Log()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("\"%s\" marked as done.\n", entry.Entry)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(doneCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// doneCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// doneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/todo.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	rapidlog "github.com/49pctber/rapidlog/internal"
+	"github.com/spf13/cobra"
+)
+
+// todoCmd represents the todo command
+var todoCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "list your todo list items",
+	Long:  `list your todo list items`,
+	Run: func(cmd *cobra.Command, args []string) {
+		rows, err := rapidlog.GetEntries(".", "100 years")
+		if err != nil {
+			panic(err)
+		}
+		for rows.Next() {
+			var entry rapidlog.Entry
+			rows.Scan(&entry.Id, &entry.Timestamp, &entry.Type, &entry.Entry)
+			fmt.Printf("\xE2\x98\x90 %s\n  [%s]\n\n", entry.Entry, entry.Id)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(todoCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// todoCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// todoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
